4c1dfffca3c120007afeca6271e53a3026d38c81
kross <cristemperatum@gmail.com>
2021-12-21 13:23:12 -0600

    ejercicio7

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 /* eslint-disable no-return-assign */
 
 // Source: https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
-
-function simpleArraySum(ar: Array<number>): number {
-  let suma = 0;
-  ar.forEach((element) => suma += element);
-  return suma;
+function staircase(num: number): void {
+let staircase:string = '';
+  for (let index = 1; index <= num; index++) {
+    for (let j = num - 1; j >= index; j--) {
+      staircase += ' ';
+    }
+    for (let k = 0; k < index; k++) {
+      staircase += '#';
+    }
+    staircase += '\n';
+  }
+  
+  console.log(staircase);
 }
-
 function main() {
   console.log('---------- Cool Programing ----------\n');
-  const testArray = [1, 2, 3, 4, 10, 11];
-  const result = simpleArraySum(testArray);
+  const n = 6;
+  const result = staircase(n);
   console.log('Result: ', result);
 }
 
